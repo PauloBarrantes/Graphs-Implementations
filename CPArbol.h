51b1ba6fa47e909497b58438b8a7e7c9823e5610
Ducan-Ally <Fakebian98@gmail.com>
2017-12-02 21:18:28 -0600

    CP documentado

--- CPArbol.h
+++ CPArbol.h
@@ -18,19 +18,39 @@ class CPArbol{
 				T elemento;
 				int prioridad;
 
+				/*!
+				   \brief "Constructor de la clase Par"
+				   \param "No recibe parametros"
+				   \return "No retorna nada"
+				*/
 				Par(){
 					prioridad = 0;
 				}
 
+				/*!
+				   \brief "Constructor de la clase Par"
+				   \param "Recibe el elemento y su prioridad"
+				   \return "No retorna nada"
+				*/
 				Par(T elemento,int prioridad){
 					this->elemento = elemento;
 					this->prioridad = prioridad;
 				}
 
+				/*!
+				   \brief "Destructor de la clase Par"
+				   \param "No recibe parametros"
+				   \return "No retorna nada"
+				*/
 				~Par(){
 
 				}
 
+				/*!
+				   \brief "Metodo sobrecargado de igual"
+				   \param "Recibe un objeto tio Par"
+				   \return "Retorna un puntero a este Par"
+				*/
 				Par* operator=(const Par& otro){
 					this->elemento = otro.elemento;
 					this->prioridad = otro.prioridad;
@@ -40,23 +60,48 @@ class CPArbol{
 
 
 	public:
+		/*!
+			 \brief "Constructor de la clase CPArbol"
+			 \param "No recibe parametros"
+			 \return "No retorna nada"
+		*/
 		CPArbol(){
 			ultimo = 0;
 			arbolVecPar = new Par[Q];
 		}
 
+		/*!
+			 \brief "Destructor de la clase CPArbol"
+			 \param "No recibe parametros"
+			 \return "No retorna nada"
+		*/
 		~CPArbol(){
 			delete arbolVecPar;
 		}
 
+		/*!
+			 \brief "Vacía la cola de prioridad"
+			 \param "No recibe parametros"
+			 \return "No retorna nada"
+		*/
 		void vaciar(){
 			ultimo = 0;
 		}
 
+		/*!
+			 \brief "devuelve verdadero si la cola está vacía"
+			 \param "No recibe parametros"
+			 \return "No retorna nada"
+		*/
 		int vacia(){
 			return !ultimo;
 		}
 
+		/*!
+			 \brief "Agrega el elemento en la posición que le corresponde"
+			 \param "recibe un elemento y su prioridad"
+			 \return "No retorna nada"
+		*/
 		void agregar(T elemento,int prioridad){
 			++ultimo;
 
@@ -72,6 +117,11 @@ class CPArbol{
 			}
 		}
 
+		/*!
+			 \brief "Devuelve el elemento que tiene la mayor prioridad en la cola"
+			 \param "No recibe parametros"
+			 \return "Retorna un elemento"
+		*/
 		T sacar(){
 			Par cajaSalida = arbolVecPar[1];
 			arbolVecPar[1] = arbolVecPar[ultimo];
@@ -105,6 +155,11 @@ class CPArbol{
 			return cajaSalida.elemento;
 		}
 
+		/*!
+			 \brief "Dice sí ya el elemento está agregado en la cola"
+			 \param "Recibe un elemento"
+			 \return "Retorna un int booleano"
+		*/
 		int pertenece(T elemento){
 			int siPert = 0;
 			if(!vacia()){
@@ -121,6 +176,11 @@ class CPArbol{
 			return ultimo;
 		}
 
+		/*!
+			 \brief "Imprime lo que hay en la cola"
+			 \param "Recibe un ostream"
+			 \return "Retorna un ostream"
+		*/
 		std::ostream& imprimir(std::ostream& salida){
 			for(int i = 1; i <= ultimo; ++i){
 				salida << "(" << arbolVecPar[i].elemento << "," << arbolVecPar[i].prioridad << ") - ";
